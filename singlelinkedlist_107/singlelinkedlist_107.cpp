b95d0acd6eabba2cebd5c393708d596ab8683a3c
rezaikram <muhammadikramriza@gmail.com>
2023-05-18 20:32:55 +0700

    pengulangan while

--- singlelinkedlist_107/singlelinkedlist_107.cpp
+++ singlelinkedlist_107/singlelinkedlist_107.cpp
@@ -41,4 +41,10 @@ void addNode() {
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
-	
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
